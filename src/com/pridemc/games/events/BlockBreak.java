f0ba8de80ebe3a7d2eee0ea6dceb36ca5d7651f7
Chris H <zrenfire@gmail.com>
2012-06-14 16:34:29 -0400

    Cleaned up legacy code that was for reverting blocks.

--- src/com/pridemc/games/events/BlockBreak.java
+++ src/com/pridemc/games/events/BlockBreak.java
@@ -2,25 +2,14 @@ package com.pridemc.games.events;
 
 import com.pridemc.games.arena.Arena;
 import com.pridemc.games.arena.ArenaManager;
-import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BlockBreak implements Listener{
-	
-	private Map<Location, Integer> broken = new HashMap<Location, Integer>();
-	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
-		
-		Block block = event.getBlock();
-
 		Player player = event.getPlayer();
 
 		if (player.hasPermission("pridegames.admin"))
@@ -28,12 +17,9 @@ public class BlockBreak implements Listener{
 
 		if (ArenaManager.isInArena(player.getName())) {
 			Arena arena = ArenaManager.getArenaPlayerIsIn(player.getName());
-			if (arena.getState().canEditBlocks()) { // The arena is allowing editing
-				broken.put(block.getLocation(), block.getTypeId()); //TODO
-				return;
+			if (!arena.getState().canEditBlocks()) { // The arena is not allowing editing
+				event.setCancelled(true);
 			}
 		}
-
-		event.setCancelled(true);
 	}
 }
--- src/com/pridemc/games/events/BlockPlace.java
+++ src/com/pridemc/games/events/BlockPlace.java
@@ -2,25 +2,15 @@ package com.pridemc.games.events;
 
 import com.pridemc.games.arena.Arena;
 import com.pridemc.games.arena.ArenaManager;
-import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BlockPlace implements Listener {
-	
-	private Map<Location, Integer> placed = new HashMap<Location, Integer>();
 	
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event){
-		
-		Block block = event.getBlock();
-
 		Player player = event.getPlayer();
 
 		if (player.hasPermission("pridegames.admin"))
@@ -28,14 +18,11 @@ public class BlockPlace implements Listener {
 
 		if (ArenaManager.isInArena(player.getName())) {
 			Arena arena = ArenaManager.getArenaPlayerIsIn(player.getName());
-			if (arena.getState().canEditBlocks()) { // The arena is allowing editing
-				
-				placed.put(block.getLocation(), 0); //TODO
-				return;
-				
+			if (!arena.getState().canEditBlocks()) { // The arena is not allowing editing
+				event.setCancelled(true);
 			}
 		}
 
-		event.setCancelled(true);
+
 	}
 }
