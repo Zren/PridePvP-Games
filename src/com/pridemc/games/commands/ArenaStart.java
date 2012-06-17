d6d79cdca4338028925619e2f7c364756755fd15
Chris H <zrenfire@gmail.com>
2012-06-17 04:41:58 -0400

    Added /arena start that will shortcut to the arena the player is currently in.

--- src/com/pridemc/games/commands/ArenaStart.java
+++ src/com/pridemc/games/commands/ArenaStart.java
@@ -6,36 +6,42 @@ import com.pridemc.games.arena.MessageUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ArenaStart {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
+		Arena arena = null;
 		if (args.length < 2) {
-			MessageUtil.sendMsg(sender, ChatColor.RED + "Please specify an arena.");
+			if (sender instanceof Player) {
+				Player player = (Player)sender;
+				arena = ArenaManager.getArenaPlayerIsIn(player.getName());
+			}
+
+			if (arena == null) {
+				// Wasn't a player in an arena.
+				MessageUtil.sendMsg(sender, ChatColor.RED + "Please specify an arena.");
+				return true;
+			}
 		} else {
 			String arenaName = args[1];
-
-			Arena arena = ArenaManager.getArena(arenaName);
-
+			arena = ArenaManager.getArena(arenaName);
 			if (arena == null) {
 				String msg = ChatColor.RED + "There is no arena called '%s'.";
 				MessageUtil.sendMsg(sender, msg, arenaName);
-			} else {
-				// Cleanup any currently running tasks
-				arena.getTaskInjector().cancelAll();
-
-				switch (arena.getState()) {
-					case WAITING_FOR_PLAYERS:
-					case COUNTING_DOWN:
-						arena.startTaskFor(Arena.State.INITIAL_GRACE_PERIOD);
-						break;
-
-					case INITIAL_GRACE_PERIOD:
-						arena.startTaskFor(Arena.State.RUNNING_GAME);
-						break;
-				}
+				return true;
 			}
+		}
+
+		switch (arena.getState()) {
+			case WAITING_FOR_PLAYERS:
+			case COUNTING_DOWN:
+				arena.startTaskFor(Arena.State.INITIAL_GRACE_PERIOD);
+				break;
+
+			case INITIAL_GRACE_PERIOD:
+				arena.startTaskFor(Arena.State.RUNNING_GAME);
+				break;
 		}
 
 		return true;
